e5c18d3fc59ca91e389ce5d7da7c07c17087fe52
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-06-05 17:05:20 -0500

    updating login example

--- packages/do-floatpod-automation/start/loginExample.js
+++ packages/do-floatpod-automation/start/loginExample.js
@@ -1,5 +1,9 @@
-module.exports.apiKey = 'as,dfakdjf0i2j0oiqdj0if3i0jf2f';
-module.exports.pod1Url = 'http://192.168.0.1/api';
-module.exports.pod2Url = 'http://192.168.0.2/api';
-module.exports.pod3Url = 'http://192.168.0.3/api';
-module.exports.cabinUrl = 'http://192.168.0.11/api';
+module.exports.apiKey = 'keyGoesHere';
+
+module.exports.floatDevices = {
+    pod2:{
+        url:'http://192.168.1.123/api',
+        inSession:true,
+        minutes:0
+    }
+}
