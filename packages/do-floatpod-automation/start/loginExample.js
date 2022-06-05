68fa6aa597ab18cff9d0740c19d163b4e273cf30
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-06-05 17:10:38 -0500

    keeping track of active session

--- packages/do-floatpod-automation/start/loginExample.js
+++ packages/do-floatpod-automation/start/loginExample.js
@@ -3,7 +3,7 @@ module.exports.apiKey = 'keyGoesHere';
 module.exports.floatDevices = {
     pod2:{
         url:'http://192.168.1.123/api',
-        inSession:true,
+        inActiveSession:false,
         minutes:0
     }
 }
