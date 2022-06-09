41ded6e59b875dafaffe49dd004cd760452da673
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-06-09 11:19:59 -0500

    fixing bug with timeout mins

--- loginExample.js
+++ loginExample.js
@@ -5,6 +5,9 @@ module.exports.floatDevices = {
         url:'http://192.168.0.123/api',
         minutesInSession:0,
         fanOnUrl:'https://maker.ifttt.com/trigger/asdfasdfasdf/with/key/asdfasdfasdf',
-        fanOffUrl:'https://maker.ifttt.com/trigger/asdfasdfasdf/with/key/asdfasdfasdf'
+        fanOffUrl:'https://maker.ifttt.com/trigger/asdfasdfasdf/with/key/asdfasdfasdf',
+        isNewSession: true,
+        timeout: null,
+        timeoutMins: 0
     }
 }
