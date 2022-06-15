013897d09476eba979366a35f21bd27e8437c5bd
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-06-15 14:50:46 -0500

    set default color when turning cabin light on/off

--- lightFanService.js
+++ lightFanService.js
@@ -2,15 +2,15 @@ module.exports = function(got,logger) {
 
     async function turnLightOn(deviceName, floatDevice){
         logger.info(`turning ${deviceName} light on`)
-        await got.get(floatDevice.lightOnUrl);    }
+        await got.get(floatDevice.lightColorDefaultUrl);
+        await got.get(floatDevice.lightOnUrl);    
+    }
 
     async function turnLightOff(deviceName, floatDevice){
         logger.info(`turning ${deviceName} light off`)
         await got.get(floatDevice.lightOffUrl);
         floatDevice.needToTurnOffPreFloatLight = false;
     }
-
-
 
     async function lightAndFanOnOffPostSessionTimer(deviceName, floatDevice){
         logger.debug("turnLightAndFanOnOffTimer", deviceName);
@@ -25,7 +25,8 @@ module.exports = function(got,logger) {
             logger.info(`${deviceName}: turning fan off after ${floatDevice.postSessionLightFanTimeoutMins}`);
             await got.get(floatDevice.fanOffUrl);
             //reset light to original color
-            got.get(floatDevice.lightOffUrl);
+            await got.get(floatDevice.lightColorDefaultUrl);
+            await got.get(floatDevice.lightOffUrl);
 
             }, floatDevice.postSessionLightFanTimeoutMins * 60 * 1000)
         // }, 0 * 60 * 1000)
