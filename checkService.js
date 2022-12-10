9ac9dcc6ae1927d49ab2d0c55eb963ae6b8ac93b
Connor Knabe <Connor-Knabe@users.noreply.github.com>
2022-12-10 12:45:39 -0600

    checks if one device is not in session and others are then alert

--- checkService.js
+++ checkService.js
@@ -1,81 +1,118 @@
 module.exports = function(got,logger,options,lightFanService) {
-        async function checkFloatStatus(deviceName,floatDevice,floatStatus){
-            // logger.debug(`${deviceName}: floatStatus ${JSON.stringify(floatStatus)}`);
-            const deviceNewSession = floatStatus.status == 1 || floatStatus.status == 2;
-            const deviceActiveSession = floatStatus.status==3;
-            const idleScreen = floatStatus.status == 0;
-           
-            if(deviceActiveSession){
-                const minsToPlayMusicBeforeEndSession = Number(floatStatus.music_pre_end) > 5 ? Number(floatStatus.music_pre_end) : 5;
-                const sessionDelayBefore = Number(floatStatus.session_delay_before) > 0 ? Number(floatStatus.session_delay_before)/60 : 1;
-                logger.debug(`${deviceName}: sessionDelayBefore ${sessionDelayBefore}`);
-                //start automation 1 minute after music starts
-                // const minsWhenSessionEnds = floatStatus.duration/60 - minsToPlayMusicBeforeEndSession + sessionDelayBefore;
-                const minsWhenSessionEnds = floatStatus.duration/60 - minsToPlayMusicBeforeEndSession + 1;
-                const activeSessionNonLast5Min = floatStatus.duration/60 != 5;
-        
-                logger.debug(`${deviceName}: mins in session ${floatDevice.minutesInSession}`);
-                logger.debug(`${deviceName}: music will play ${minsToPlayMusicBeforeEndSession} mins before session over`);
-                logger.debug(`${deviceName}: mins when session ends ${minsWhenSessionEnds}`);
-                logger.debug(`${deviceName}: duration mins ${floatStatus.duration/60}`);
-        
-                if(activeSessionNonLast5Min){
-                    if(floatDevice.minutesInSession >= minsWhenSessionEnds){
-                        logger.info(`${deviceName}: turning light and fan on end of session`);
-                        await lightFanService.lightAndFanOnOffPostSessionTimer(deviceName,floatDevice);
-                        floatDevice.minutesInSession = 1;
-                    } else if (floatDevice.minutesInSession == 0) {
-                        logger.info(`${deviceName}: turning fan off 0 mins into active session`);
-                        lightFanService.turnFanOff(deviceName, floatDevice);
-                        lightFanService.turnLightOff(deviceName, floatDevice);
-                        floatDevice.minutesInSession = 1
-                    }
-                    logger.debug(`${deviceName}: floatDevice.minutesInSession ${floatDevice.minutesInSession}`);
+    var shouldAlertDeviceInSession = true;
+    async function checkFloatStatus(deviceName,floatDevice,floatStatus, silentStatus){
+        // logger.debug(`${deviceName}: floatStatus ${JSON.stringify(floatStatus)}`);
+        const deviceNewSession = floatStatus.status == 1 || floatStatus.status == 2;
+        const deviceActiveSession = floatStatus.status==3;
+        const idleScreen = floatStatus.status == 0;
+        floatDevice.status = floatStatus.status;
+        floatDevice.silentStatus = silentStatus;
 
-                    floatDevice.minutesInSession++;
-                } else if(floatDevice.minutesInSession > -1){
-                    logger.info(`${deviceName} turning light and fan on manual 5 min timer`);
-                    await lightFanService.lightAndFanOnOffPostSessionTimer(deviceName, floatDevice);
-                    floatDevice.minutesInSession = -1;
-                }
-            } else if (deviceNewSession){
-                //only want to turn off fan once when in new session screen
-                logger.debug(`${deviceName}: mins in session now ${floatDevice.minutesInSession}`);
-                if(floatDevice.minutesInSession==0){
-                    logger.info(`${deviceName}: turning fan off when in new session screen`);
+        if(deviceActiveSession){
+            const minsToPlayMusicBeforeEndSession = Number(floatStatus.music_pre_end) > 5 ? Number(floatStatus.music_pre_end) : 5;
+            const sessionDelayBefore = Number(floatStatus.session_delay_before) > 0 ? Number(floatStatus.session_delay_before)/60 : 1;
+            logger.debug(`${deviceName}: sessionDelayBefore ${sessionDelayBefore}`);
+            //start automation 1 minute after music starts
+            // const minsWhenSessionEnds = floatStatus.duration/60 - minsToPlayMusicBeforeEndSession + sessionDelayBefore;
+            const minsWhenSessionEnds = floatStatus.duration/60 - minsToPlayMusicBeforeEndSession + 1;
+            const activeSessionNonLast5Min = floatStatus.duration/60 != 5;
+    
+            logger.debug(`${deviceName}: mins in session ${floatDevice.minutesInSession}`);
+            logger.debug(`${deviceName}: music will play ${minsToPlayMusicBeforeEndSession} mins before session over`);
+            logger.debug(`${deviceName}: mins when session ends ${minsWhenSessionEnds}`);
+            logger.debug(`${deviceName}: duration mins ${floatStatus.duration/60}`);
+
+    
+            if(activeSessionNonLast5Min){
+                if(floatDevice.minutesInSession >= minsWhenSessionEnds){
+                    logger.info(`${deviceName}: turning light and fan on end of session`);
+                    await lightFanService.lightAndFanOnOffPostSessionTimer(deviceName,floatDevice);
+                    floatDevice.minutesInSession = 1;
+                } else if (floatDevice.minutesInSession == 0) {
+                    logger.info(`${deviceName}: turning fan off 0 mins into active session`);
                     lightFanService.turnFanOff(deviceName, floatDevice);
                     lightFanService.turnLightOff(deviceName, floatDevice);
-                    floatDevice.minutesInSession = 1;
+                    floatDevice.minutesInSession = 1
                 }
-                await checkForOverNightSession(deviceName, floatDevice);
+                logger.debug(`${deviceName}: floatDevice.minutesInSession ${floatDevice.minutesInSession}`);
 
-            } else if (idleScreen) {
-                // logger.debug(`${deviceName}: no session active screen.`);
-                floatDevice.minutesInSession = 0;
+                floatDevice.minutesInSession++;
+            } else if(floatDevice.minutesInSession > -1){
+                logger.info(`${deviceName} turning light and fan on manual 5 min timer`);
+                await lightFanService.lightAndFanOnOffPostSessionTimer(deviceName, floatDevice);
+                floatDevice.minutesInSession = -1;
+            }
+        } else if (deviceNewSession){
+            //only want to turn off fan once when in new session screen
+            logger.debug(`${deviceName}: mins in session now ${floatDevice.minutesInSession}`);
+            if(floatDevice.minutesInSession==0){
+                logger.info(`${deviceName}: turning fan off when in new session screen`);
+                lightFanService.turnFanOff(deviceName, floatDevice);
+                lightFanService.turnLightOff(deviceName, floatDevice);
+                floatDevice.minutesInSession = 1;
+            }
+            await checkForOverNightSession(deviceName, floatDevice);
+
+        } else if (idleScreen) {
+            // logger.debug(`${deviceName}: no session active screen.`);
+            floatDevice.minutesInSession = 0;
+            await checkForAllDevicesInSession(floatDevice);
+        }
+    }
+    async function checkForOverNightSession(deviceName, floatDevice){
+        const theTime = new Date();
+        if(theTime.getHours() >= 0 && theTime.getHours() < 1){
+            logger.debug(`checkForOverNightSession time passed ${floatDevice.minutesInSession}`);
+            if(floatDevice.minutesInSession > 5){
+                //send request to take out of session
+                logger.info(`${deviceName}: taking out of session overnight`);
+                await got.post(floatDevice.url, {
+                    form:{
+                        "api_key": options.apiKey,
+                        "command":"set_session_cancel"
+                    }
+                });
+            } else {
+                floatDevice.minutesInSession++;
+                logger.debug(`checkForOverNightSession mins in session ${floatDevice.minutesInSession}`);
+            }
+            
+        } 
+    }   
+
+    function anyDevicesInSession(){
+        var deviceInSession = false;
+        var count = 0;
+        for (var key in options.floatDevices) {
+            if (options.floatDevices.hasOwnProperty(key)) {
+                var floatDevice = options.floatDevices[key];
+                //TODO: check for silent mode as well
+                if(floatDevice.status > 0 && floatDevice.silentStatus != 1){
+                    count++
+                    deviceInSession = true;
+                }
             }
         }
-        async function checkForOverNightSession(deviceName, floatDevice){
-            const theTime = new Date();
-            if(theTime.getHours() >= 0 && theTime.getHours() < 1){
-                logger.debug(`checkForOverNightSession time passed ${floatDevice.minutesInSession}`);
-                if(floatDevice.minutesInSession > 5){
-                    //send request to take out of session
-                    logger.info(`${deviceName}: taking out of session overnight`);
-                    await got.post(floatDevice.url, {
-                        form:{
-                            "api_key": options.apiKey,
-                            "command":"set_session_cancel"
-                        }
-                    });
-                } else {
-                    floatDevice.minutesInSession++;
-                    logger.debug(`checkForOverNightSession mins in session ${floatDevice.minutesInSession}`);
-                }
-                
-            } 
-        }        
-
-        return {
-            checkFloatStatus: checkFloatStatus
+        if(count==4){
+            shouldAlertDeviceInSession = true;
         }
+        return deviceInSession;
+    }
+    
+    async function checkForAllDevicesInSession(floatDevice){
+        var areDevicesInSession = anyDevicesInSession();
+        if(areDevicesInSession){
+            //send alert
+            shouldAlertDeviceInSession = false;
+            await got.post(options.ifttt.alertUrl, {
+                json: {
+                    value1: floatDevice
+                }
+            });
+        }
+    }
+
+    return {
+        checkFloatStatus: checkFloatStatus
+    }
 };
--- cronService.js
+++ cronService.js
@@ -8,46 +8,62 @@ module.exports = function(options,got,logger, lightFanService) {
         async () => {
             for (var key in options.floatDevices) {
                 if (options.floatDevices.hasOwnProperty(key)) {
-                var floatDevice = options.floatDevices[key];
-                try{
-                    /*
-                    * Commands:
-                    "ping",
-                    "get_water_temperature",
-                    "get_controller_temperature",
-                    "get_relay_status",
-                    "get_silence_status",
-                    "set_silence_on",
-                    "set_silence_off",
-                    "get_session_status",
-                    "set_session_start",
-                    "set_session_cancel",
-                    */
-                    const data = await got.post(options.floatDevices[key].url, {
-                        form:{
-                        "api_key": options.apiKey,
-                        "command":"get_session_status"
-                        }
-                    });
-            
+                    var floatDevice = options.floatDevices[key];
                     try {
-                        var floatStatus = data ? JSON.parse(data.body) : null;
-                        floatStatus = floatStatus ? JSON.parse(floatStatus.msg) : null;
-                        got.get(options.floatDevices[key].healthCheckUrl);
+                        /*
+                        * Commands:
+                        "ping",
+                        "get_water_temperature",
+                        "get_controller_temperature",
+                        "get_relay_status",
+                        "get_silence_status",
+                        "set_silence_on",
+                        "set_silence_off",
+                        "get_session_status",
+                        "set_session_start",
+                        "set_session_cancel",
+                        */
+                        const data = await got.post(options.floatDevices[key].url, {
+                            form:{
+                            "api_key": options.apiKey,
+                            "command":"get_session_status"
+                            }
+                        });
+                
+                        try {
+                            var floatStatus = data ? JSON.parse(data.body) : null;
+                            floatStatus = floatStatus ? JSON.parse(floatStatus.msg) : null;
+                        } catch (ex){
+                            logger.error(`${key}: failed to parse float status response ${ex}`)
+                        }
+
+                        const silentData = await got.post(options.floatDevices[key].url, {
+                            form:{
+                            "api_key": options.apiKey,
+                            "command":"get_session_status"
+                            }
+                        });
+                        
+
+                        var silentStatus = null;
+                        try {
+                            silentStatus = silentData ? JSON.parse(silentData.body) : null;
+                            silentStatus = silentStatus ? silentStatus.msg : null;
+                        } catch (ex){
+                            logger.error(`${key}: failed to parse silent status response ${ex}`)
+                        }
+
+                        if(floatStatus){
+                            checkService.checkFloatStatus(key,floatDevice,floatStatus,silentStatus);
+                            got.get(options.floatDevices[key].healthCheckUrl);
+                        } else {
+                            logger.error(`${key}: couldn't find float status`);
+                        }
                     } catch (ex){
-                        logger.error(`${key}: failed to parse float status response ${ex}`)
+                        logger.error('Failed to get status', ex);
                     }
-        
-                    
-                } catch (ex){
-                    logger.error('Failed to get status', ex);
-                }
-               
-                if(floatStatus){
-                    checkService.checkFloatStatus(key,floatDevice,floatStatus);
-                } else {
-                    logger.error(`${key}: couldn't find float status`);
-                }
+                
+
                 }
             }
         },
